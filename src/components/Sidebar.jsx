214c0953a57cee96557c13f470cfdfb83a232218
denizdag14 <dnxdag@gmail.com>
2024-06-19 21:41:11 +0300

    modify the sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,20 @@
+import { FaTwitter } from 'react-icons/fa'
+import { HiHome } from 'react-icons/hi'
+import Link from 'next/link'
+
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col gap-4 p-3'>
+        <Link href='/'>
+            <FaTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 hover:rounded-3xl transition-all duration-200 text-blue-500' />
+        </Link>
+        <Link href='/' className='flex items-center p-3 hover:bg-gray-100 hover:rounded-3xl transition-all duration-200 gap-2 w-fit'>
+            <HiHome className='w-7 h-7' />
+            <span className='font-bold hidden xl:inline'>Home</span>
+        </Link>
+        <button className='bg-blue-400 text-white rounded-full hover:bg-blue-500 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline'>
+            Sign In
+        </button>
+    </div>
   )
 }
