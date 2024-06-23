a840b807d4425c3264221f3f7ab9fb4623d95bc3
denizdag14 <dnxdag@gmail.com>
2024-06-23 23:52:01 +0300

    update postpage to display post details

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Feed from "@/components/Feed";
 export default function page() {
   return (
     <div className='max-w-xl mx-auto border-r border-l dark:border-zinc-800 min-h-screen'>
-      <div className='py-2 px-3 sticky top-0 z-50 border-b dark:border-zinc-800 border-gray-200'>
+      <div className='py-2 px-3 sticky top-0 border-b dark:border-zinc-800 border-gray-200'>
         <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
       </div>
       <HomeInput />
--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,5 +1,25 @@
-export default function PostPage() {
+import { app } from "@/firebase";
+import { getDoc, getFirestore, doc, query } from "firebase/firestore";
+import { HiArrowLeft } from "react-icons/hi";
+import Link from "next/link";
+import ForPostPage from "@/components/ForPostPage";
+
+export default async function PostPage({params}) {
+
+  const db = getFirestore(app);
+  let data = {};
+  const querySnapshot = await getDoc(doc(db, 'posts', params.id));
+  data = { ...querySnapshot.data(), id: querySnapshot.id };
+
   return (
-    <div>PostPage</div>
+    <div className="max-w-xl mx-auto border-r border-l dark:border-zinc-800 min-h-screen">
+      <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 bg-white border-b border-gray-200 dark:border-zinc-800 dark:bg-zinc-900">
+        <Link href={'/'} className="hover:bg-gray-100 dark:hover:bg-zinc-800 rounded-full p-2" >
+          <HiArrowLeft className="h-5 w-5" />
+        </Link>
+        <h2 className="sm:text-lg">Back</h2>
+      </div>
+      <ForPostPage post={data} id={data.id}/>
+    </div>
   )
 }
