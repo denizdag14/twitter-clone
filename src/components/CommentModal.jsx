b6baf8fb9ac87f75782d55edecd76e2d26af038a
denizdag14 <dnxdag@gmail.com>
2024-06-23 23:01:09 +0300

    add comment functionality to the commentModal component

--- src/components/CommentModal.jsx
+++ src/components/CommentModal.jsx
@@ -3,22 +3,87 @@
 import { useRecoilState } from 'recoil'
 import { modalState, postIdState } from '@/atom/modalAtom'
 import Modal from 'react-modal'
-import { HiX } from 'react-icons/hi'
+import { HiX, HiOutlinePhotograph } from 'react-icons/hi'
 import { useSession } from 'next-auth/react'
-import { useEffect, useState } from 'react'
-import { doc, getFirestore, onSnapshot } from 'firebase/firestore'
+import { useEffect, useState, useRef } from 'react'
+import { getFirestore, onSnapshot, addDoc, serverTimestamp, doc, collection } from 'firebase/firestore'
+import { getStorage, ref, getDownloadURL, uploadBytesResumable} from 'firebase/storage';
 import { app } from '@/firebase'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Textarea } from '@headlessui/react'
+import { CircularProgress } from '@mui/material'
+import { useRouter } from 'next/navigation'
 
 export default function CommentModal() {
+    const filePickRef = useRef(null);
+    const [fileUrl, setFileUrl] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [fileUploading, setFileUploading] = useState(null);
+    const [fileType, setFileType] = useState(null);
     const {data: session} = useSession();
     const db = getFirestore(app);
     const [open, setOpen] = useRecoilState(modalState);
+    const [uploadProgress, setUploadProgress] = useState(0);
     const [postId, setPostId] = useRecoilState(postIdState);
     const [post, setPost] = useState({});
     const [input, setInput] = useState('');
+    const [postLoading, setPostLoading] = useState(false);
+    const router = useRouter();
+
+    const addFileToPost = (e) => {
+      const file = e.target.files[0];
+      if (file) {
+        setSelectedFile(file);
+        setFileType(file.type.split("/")[0]);
+        setFileUrl(URL.createObjectURL(file));
+      }
+    };
+
+    const uploadFileToStorage = () => {
+
+      return new Promise((resolve, reject) => {
+        setFileUploading(true);
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + "-" + selectedFile.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setUploadProgress(progress);
+            console.log("Upload is " + progress + "% done");
+          },
+          (error) => {
+            console.log(error);
+            setFileUploading(false);
+            setFileUrl(null);
+            setSelectedFile(null);
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setFileUrl(downloadURL);
+              setFileUploading(false);
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    useEffect(() => {
+      if (open) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'auto';
+      }
+      
+      return () => {
+        document.body.style.overflow = 'auto';
+      }
+    }, [open]);
 
     useEffect(() => {
       if(postId !== ''){
@@ -38,7 +103,30 @@ export default function CommentModal() {
     }, [postId]);
 
     const sendComment = async () => {
-      
+      setPostLoading(true);
+
+      let uploadedFileUrl = null;
+
+      if (selectedFile) {
+        uploadedFileUrl = await uploadFileToStorage();
+      }
+
+      const docRef = await addDoc(collection(db, 'posts', postId, 'comments'), {
+        name: session.user.name,
+        username: session.user.username,
+        comment: input,
+        profileImg: session.user.image,
+        timestamp: serverTimestamp(),
+        file: uploadedFileUrl,
+        fileType: fileType,
+      }).then(() => {
+        setPostLoading(false);
+        setOpen(false);
+        setInput('');
+        setFileUrl(null);
+        setSelectedFile(null);
+        router.push(`/posts/${postId}`)
+      }).catch(err => {console.error(err)});
     }
 
   return (
@@ -51,8 +139,9 @@ export default function CommentModal() {
               ariaHideApp={false}
               className='max-w-lg w-[90%] absolute top-24 left-[50%] translate-x-[-50%] bg-white dark:bg-zinc-900 rounded-xl shadow-2xl outline-none'
             >
-              <div className='p-4'>
-                <div className='border-b dark:border-zinc-800 border-gray-200 py-2 px-1.5 flex justify-end'>
+              <div className='p-4 max-h-[80vh] overflow-y-auto'>
+                <div className='sticky top-0 dark:bg-zinc-800 bg-gray-200 rounded-xl border-b dark:border-zinc-800 border-gray-200 py-2 px-1.5 flex justify-between z-10'>
+                  <span className='ml-4'>Leave a comment</span>
                   <HiX className='text-2xl text-gray-700 p-1 dark:hover:bg-zinc-800 hover:bg-gray-200 rounded-full cursor-pointer' onClick={() => setOpen(false)}/>
                 </div>
                 <div className='p-2 flex items-center space-x-1 relative'>
@@ -81,9 +170,41 @@ export default function CommentModal() {
                     <div className='w-full'>
                       <Textarea className='w-full border-none outline-none tracking-wide min-h-12 dark:bg-zinc-900 placeholder:text-gray-500' placeholder='Whats happening' rows='2' value={input} onChange={(e) => setInput(e.target.value)}>
                       </Textarea>
+                      {selectedFile && fileType === "image" && (
+                        fileUploading ? (
+                          <div className="rounded-full border-none w-auto h-auto flex items-center justify-center mb-2">
+                            <CircularProgress variant="determinate" value={uploadProgress}/>
+                          </div>
+                        ) : (
+                          <Image
+                            src={fileUrl}
+                            alt="file"
+                            className="w-auto h-auto object-cover rounded-md mb-2"
+                            width={50}
+                            height={50}
+                          />
+                        )
+                      )}
+                      {selectedFile && fileType === "video" && (
+                        fileUploading ? (
+                          <div className="rounded-full border-none w-auto h-auto flex items-center justify-center mb-2">
+                            <CircularProgress variant="determinate" value={uploadProgress}/>
+                          </div>
+                        ) : (
+                          <video
+                            src={fileUrl}
+                            controls
+                            className="w-auto h-auto object-cover cursor-pointer rounded-md mb-2"
+                            width={50}
+                            height={50}
+                          />
+                        )
+                      )}
+                      <HiOutlinePhotograph onClick={() => filePickRef.current.click()} className='h-10 w-10 p-2 text-sky-500 dark:hover:bg-sky-950 hover:bg-sky-100 rounded-full cursor-pointer' />
+                      <input hidden type='file' ref={filePickRef} accept='image/*,video/*' onChange={addFileToPost}/>
                     </div>
                     <div className='flex items-center justify-end pt-2.5'>
-                      <button className='bg-blue-400 disabled:hover:bg-blue-400 hover:bg-blue-500 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:brightness-100 disabled:opacity-50' disabled={input.trim() === ''} onClick={sendComment}>
+                      <button className='bg-blue-400 disabled:hover:bg-blue-400 hover:bg-blue-500 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:brightness-100 disabled:opacity-50' disabled={input.trim() === '' && !selectedFile || postLoading || fileUploading} onClick={sendComment}>
                         Reply
                       </button>
                     </div>
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -15,6 +15,7 @@ export default function Icons({id, uid, isTrash}) {
     const [open, setOpen] = useRecoilState(modalState);
     const [postId, setPostId] = useRecoilState(postIdState);
     const db = getFirestore(app);
+    const [comments, setComments] = useState([]);
     const likePost = async () => {
 
         if(session){
@@ -50,6 +51,13 @@ export default function Icons({id, uid, isTrash}) {
         setIsLiked(likes.findIndex((like) => like.id === session?.user?.uid) !== -1);
     }, [likes]);
 
+    useEffect(() => {
+        const unsubscribe = onSnapshot(collection(db, 'posts', id, 'comments'), 
+        (snapshot) => setComments(snapshot.docs)
+        )
+        return () => unsubscribe();
+    }, [db, id])
+
     const deletePost = async () => {
         if(window.confirm('Are you sure you want to delete this post?')) {
             if(session?.user?.uid === uid) {
@@ -64,6 +72,8 @@ export default function Icons({id, uid, isTrash}) {
             }
         }
     };
+
+    console.log(comments);
 
   return (
     <>
@@ -85,15 +95,23 @@ export default function Icons({id, uid, isTrash}) {
             )
         ) : (
             <div className="flex justify-start gap-5 p-2 text-gray-500">
-                <HiOutlineChat className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100 dark:hover:bg-sky-950" 
-                onClick={() => {
-                    if(!session){
-                        signIn();
-                    }else{
-                        setOpen(!open);
-                        setPostId(id)
+                <div className="flex items-center">
+                    <HiOutlineChat className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100 dark:hover:bg-sky-950" 
+                    onClick={() => {
+                        if(!session){
+                            signIn();
+                        }else{
+                            setOpen(!open);
+                            setPostId(id)
+                        }
+                    }} />
+                    {
+                        comments.length > 0 && (
+                            <span className="text-xs">{comments.length}</span>
+                        )
                     }
-                }} />
+                </div>
+                
                 <div className="flex items-center">
                     {isLiked ? (
                         <HiHeart onClick={likePost} className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:bg-red-100 dark:hover:bg-red-950"/>
